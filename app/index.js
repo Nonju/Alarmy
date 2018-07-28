edecc8579bc4cef9a7dbd74a382528ac1542697c
Nonju <albinzon@gmail.com>
2018-07-29 01:53:14 +0200

    Suppress debug warnings

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
+console.disableYellowBox = true; // Disable warnings in debug-mode
 AppRegistry.registerComponent('app', () => App);
