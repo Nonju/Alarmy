a72b4d607cef683d8eb1877de5cf3ab7b68dd527
Nonju <albinzon@gmail.com>
2018-08-02 21:22:44 +0200

    Custom android toastmaster module

---
+++ app/src/component/ToastMaster.js
@@ -0,0 +1,11 @@
+
+/*
+ * Usage:
+ * - ToastMaster.show('Short toast message', ToastMaster.SHORT);
+ * - ToastMaster.show('Long toast message', ToastMaster.LONG);
+ *
+ */
+
+import { NativeModules } from 'react-native';
+export default NativeModules.ToastMaster;
+
--- app/src/screen/AlarmItemScreen.js
+++ app/src/screen/AlarmItemScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, NativeModules } from 'react-native';
 import BaseScreen from './BaseScreen';
 
 export default class AlarmItem extends BaseScreen {
